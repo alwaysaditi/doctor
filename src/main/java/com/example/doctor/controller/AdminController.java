afe175dc6979a5e629a93f89bff76be6155c6d52
alwaysaditi <aditiparetkar@gmail.com>
2023-09-11 21:00:33 +0400

    Displayed the table of users having ROLE_USER on admin access page

--- src/main/java/com/example/doctor/controller/AdminController.java
+++ src/main/java/com/example/doctor/controller/AdminController.java
@@ -1,15 +1,32 @@
 package com.example.doctor.controller;
 
+import com.example.doctor.entity.Roles;
+import com.example.doctor.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private RolesRepository rolesRepository;
     @GetMapping("/")
     public String adminHome()
     {
         return "admin-home";
+    }
+
+    @GetMapping("/assignusers")
+    public String assignuserstoRoles(Model model)
+    {
+        List<Roles> user_roleList = rolesRepository.user_roles();
+       model.addAttribute("userlist",user_roleList);
+
+       return "assign-roles";
     }
 }
--- src/main/java/com/example/doctor/repository/RolesRepository.java
+++ src/main/java/com/example/doctor/repository/RolesRepository.java
@@ -2,6 +2,13 @@ package com.example.doctor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.doctor.entity.Roles;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface RolesRepository extends JpaRepository<Roles,String> {
     Roles save(Roles role);
+
+    @Query(nativeQuery= true, value="SELECT * FROM roles WHERE role = 'ROLE_USER'")
+    List<Roles> user_roles();
 }
