e02fd6f43157a4a65b3909d0b9488f600aaf58e8
alwaysaditi <aditiparetkar@gmail.com>
2023-09-16 20:27:44 +0400

    Implemented few changes in role assign form and admin page

--- src/main/java/com/example/doctor/controller/PharmacistController.java
+++ src/main/java/com/example/doctor/controller/PharmacistController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/pharmacist")
 public class PharmacistController {
-    @GetMapping("/")
+    @GetMapping("/") // this is the second page after succesful login, the first being the welcome pag
+    // this page can be customised for each user. basically filling up details
     public String pharmaHome()
     {
         return "pharmacist-home";
